ab35980a6f77aa768d3c6891d2fc34bbc710cfe1
Joshua Kemp <joshuakemp85@gmail.com>
2015-02-24 09:44:19 -0500

    Completed another code challenge

---
+++ easy_coderbyte_challenges/sum_All_Numbers_In_Range.js
@@ -0,0 +1,12 @@
+// We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+
+function sumAll(arr) {
+  return(1);
+}
+
+sumAll([1, 4]);
+
+
+// (sumAll([5, 10])).to.equal(45);
+// (sumAll([4, 1])).to.equal(10);
--- easy_coderbyte_challenges/where_Do_I_Belong.js
+++ easy_coderbyte_challenges/where_Do_I_Belong.js
@@ -3,7 +3,21 @@
 
 function where(arr, num) {
   // Find my place in this sorted array.
-  return num;
-}
+  // loop over the arr and compare if the arr element is less then the num
+  // if less then keep looping, if the arr element is greater then or === to num
+  // then record that arr elements index and return -1 index less for the answer
+  console.log(arr);
+  for (var i = 0; i < arr.length; i++){
+    if ( arr[i] < num ){
+      // console.log("lower!");
+    }
+    else if ( arr[i] >= num ){
+      var position = arr[i];
+      return arr.indexOf(position);
+     }
+    }
+  }
+  // return num;
 
-where([40, 60], 50);
+console.log(where([40, 60], 50));
+// console.log((where([10, 20, 30, 40, 50], 30)));
